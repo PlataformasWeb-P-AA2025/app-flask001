8af81802b381df973630a606271a49b8a633a2d9
René Elizalde <reroes799@gmail.com>
2025-04-15 23:07:41 -0500

    actualización del mensaje

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '¡Hola, mundo! Esta app está desplegada en Render usando GitHub Actions.'
+    return '¡Hola, mundo! Esta app está desplegada en Render usando GitHub Actions. Y esto es una prueba'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=5000)
