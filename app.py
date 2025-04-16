64f2f7b14b352f4511ca793931751362a9856704
René Elizalde <reroes799@gmail.com>
2025-04-16 08:57:13 -0500

    mis cambios

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '¡Hola, mundo! Esta app está desplegada en Render usando GitHub Actions. Y esto es una prueba - <b>3</b>'
+
+    actual = datetime.now()
+    fecha_formateada = actual.strftime("%d, %B, %Y, %M, %H, %S")
+    return f'¡Hola, mundo! <b>{fecha_formateada}</b>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
